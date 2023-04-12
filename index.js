c6d23c30bfd2c9e85484fe19a72c8fb4ddbdbd54
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-12 15:48:50 -0700

    basic admin login validation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const loginButton = document.getElementById("login-button");
 const credSubmitButton = document.getElementById("submit-button");
 const loginPrompt = document.getElementById("login-prompt");
 
+const USER = "admin";
+const PASS = "admin";
 
 function display(x){
   x.style.display = "block";
@@ -12,9 +14,21 @@ function hide(x){
   x.style.display = "block";
 }
 
+function validate_admin(user, pass){
+  if (user == USER && pass == PASS){
+    alert("success");
+  } else {
+    alert("fail");
+  }
+}
+
 loginButton.addEventListener("click", function() {
   display(loginPrompt);
 });
+
 credSubmitButton.addEventListener("click", function() {
+  var user = document.getElementById("admin-username").value;
+  var pass = document.getElementById("admin-password").value;
+  validate_admin(user, pass)
   hide(loginPrompt);
 });
