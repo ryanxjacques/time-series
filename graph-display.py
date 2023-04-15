57c944649e31cb4e94a8598322a9febd13748f28
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-04-14 17:21:02 -0700

    Can Process NaN/Null vals
    data drops Na values and parses gaps

--- graph-display.py
+++ graph-display.py
@@ -5,20 +5,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandas import Series
-
+from pandas import DataFrame
 
 # this changes the default date converter for better interactive plotting of dates:
 plt.rcParams['date.converter'] = 'concise'
 
 
-# TS Data consists only Date, Time, Magnitude.
-with open("data-placeholder.csv", 'r') as f:
-    # this can change when introducing multiple scalars (SEE pandas.DataFrame)
-    data = pd.read_csv("data-placeholder.csv", header=1, names=('TIME', 'MAGNITUDE'))
-    time = data['TIME'].values
-    magnitude = data['MAGNITUDE'].values
-    length = len(data)
 
+#TODO: Open metadata first to check scalars. This will allow several magnitudes to be stored in one magnitude graph.
+# Create as many magnitudes as needed. Can update from magnitude[] to magnitude[][] to store different magnitudes.
 with open("metadata-placeholder.csv", 'r') as g:
     # This will never change with our consistent formatting
     metadata = pd.read_csv("metadata-placeholder.csv", header=None, names=('TS_NAME', 'DESCRIPTION', 'DOMAINS',
@@ -32,18 +27,21 @@ with open("metadata-placeholder.csv", 'r') as g:
     keywords = metadata['KEYWORDS'].values[1]
     scalar = int(metadata['SCALAR'].values[1])
 
-print(ts_name)
-print(description)
-print(x_domain)
-print(y_domain)
-print(units)
-print(keywords)
-print(scalar)
-print(length)
+# TS Data consists only Date, Time, Magnitude.
+with open("data-placeholder.csv", 'r') as f:
+    # this can change when introducing multiple scalars (SEE pandas.DataFrame)
+    data = pd.read_csv("data-placeholder.csv", header=1, names=('TIME', 'MAGNITUDE'))
+    data.dropna(subset=['TIME', 'MAGNITUDE'], inplace=True)
+    print(data)
+    time = data['TIME'].values
+    magnitude = data['MAGNITUDE'].values
+    length = len(data)
 
 
-
-ts = Series(magnitude, index=time)
+if scalar == 1:
+    ts = Series(magnitude, index=time)
+else:
+    ts = DataFrame(magnitude, index=time)
 
 plt.ylabel(f"{y_domain} ({units})")
 plt.xlabel(x_domain)
