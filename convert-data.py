f5d461d3eaf27343f26fe8f5e2a158f708b39221
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-04-15 18:59:27 -0700

    Split data sets for test train. Improve conversion functionality.
    Input files will now be converted into three separate .csv files: raw data, test, and train. Saved locally as "data-placeholder.csv", "test-placeholder.csv", and "train-placeholder.csv" respectively.
    Additionally, conversion between JSON files is working. TXT files still have unexpected bugs. Working on them next.

--- convert-data.py
+++ convert-data.py
@@ -25,18 +25,32 @@ read_functions = {
 }
 
 
-file_name = "TestData/placeholderfile.txt"
-dest_file = "./TestData/data-placeholder.csv"
+file_name = "TestData/placeholderfile.json"
+data_file = "./TestData/data-placeholder.csv"
+train_file = "./TestData/train-placeholder.csv"
+test_file = "./TestData/test-placeholder.csv"
 file_ext = file_name.split('.')[-1]
 
 if file_ext not in read_functions:
     raise ValueError(f"Unsupported file type {file_ext}")
 
 data = read_functions[file_ext](file_name)
+# read the data into a pd.DataFrame
 
-data.to_csv(dest_file, index=False)
+split_index = int(len(data) * 0.8)
+# index split point for test and train files
 
-print(f"File {file_name} converted to CSV and saved at {dest_file}")
+train = data.iloc[:split_index, :]
+test = data.iloc[split_index:, :]
+# separate data
+
+data.to_csv(data_file, index=False)
+train.to_csv(train_file, index=False)
+test.to_csv(test_file, index=False)
+# convert data to a csv and send it to respective file locations
+
+print(f"File {file_name} converted to CSV and saved at {data_file}\n"
+      f"Split into \"test\" and \"train\" files in the same directory.")
 
 
 
