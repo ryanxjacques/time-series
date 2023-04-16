46de0b189a5366d64050dced5c2eb6b3e0756452
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-16 15:51:29 -0700

    added AJAX GET method

---
+++ web_app/js_modules/ajax_modules/xhr.js
@@ -0,0 +1,41 @@
+// credit: youtube channel Academind - "Sending JavaScript Http Requests with XMLHttpRequest".
+
+const sentHttpRequest = (method, url, data) => {
+  // promise allows for async code.
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.responseType = 'json';
+
+    if (data) {
+      xhr.setRequestHeader('Content-Type', 'application/json');
+    }
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    }
+
+    xhr.onerror = () => {
+      reject("Something went wrong!")
+    }
+
+    xhr.send(JSON.stringify(data));
+  });
+  // to use the promise we need to return it.
+  return promise;
+}
+
+const getData = (url) => {
+  sentHttpRequest('GET', url).then(responseData => {
+    console.log(responseData);
+  });
+}
+
+// Test submit.
+getData('https://pages.uoregon.edu/jerling2/time-series/example.json');
