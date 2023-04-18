1e1e0b632b6bf49a9cbed0578ec46565b7b3b0ad
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-18 12:58:17 -0700

    patching bugs

--- server/server.js
+++ server/server.js
@@ -80,9 +80,23 @@ connection.query("SELECT username, password FROM users WHERE id=0", function (er
 // });
 
 // Register the routes with Express
-app.use('/', home);
-app.post('/', home.post);
-app.post('/login', login.post);
+// app.use('/', home);
+
+app.post('/', (req, res) => {
+  const data = JSON.parse(req.body);
+  // Here you can do whatever you want with the received data
+  
+  console.log(data.message);
+  res.send('Data received successfully!');
+});
+
+app.post('/login', (req, res) => {
+  const data = JSON.parse(req.body);
+  // Here you can do whatever you want with the received data
+  
+  console.log(data.message);
+  res.send('Data received successfully!');
+});
 
 // Listen on port 3000 (default).
 const port = process.env.PORT || 3000;
