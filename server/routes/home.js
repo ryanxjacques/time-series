dc5c6f037883284460af80f38d553058c2e51d2e
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-18 14:54:55 -0700

    patching bugs

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,46 +1,10 @@
 const express = require('express');
 const home = express.Router();
 
-// <======= NEW STUFF =======>
-const multer = require('multer');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-const imageStorage = multer.diskStorage({
-  // Destination to store image     
-  destination: 'images', 
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '_' + Date.now() 
-           + path.extname(file.originalname))
-          // file.fieldname is name of the field (image)
-          // path.extname get the uploaded file extension
-  }
-});
-
-const imageUpload = multer({
-  storage: imageStorage,
-  limits: {
-    fileSize: 1000000 // 1000000 Bytes = 1 MB
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(png|jpg)$/)) { 
-       // upload only png and jpg format
-       return cb(new Error('Please upload a Image'))
-     }
-   cb(undefined, true)
-}
-});
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads/') // specify the destination folder
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname) // use the original file name
-  }
-});
-
-const upload = multer({ storage: storage });
-// <======= END =======>
-
+const { uploadFile, getFileStream } = require('./s3')
 
 home.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -54,12 +18,20 @@ home.post('/', (req, res) => {
   res.send(JSON.stringify(msg));
 });
 
-// For Single image upload
-home.put('/', imageUpload.single('image'), (req, res) => {
-  res.send(req.file)
-}, (error, req, res, next) => {
-  res.status(400).send({ error: error.message })
-});
+home.put('/', upload.single('image'), async (req, res) => {
+  const file = req.file
+  console.log(file)
+
+  // apply filter
+  // resize 
+
+  const result = await uploadFile(file)
+  await unlinkFile(file.path)
+  console.log(result)
+  const description = req.body.description
+  res.send({imagePath: `/images/${result.Key}`})
+})
+
 
 // home.put('/', upload.single('file'), function(req, res) {
 //   console.log('recieved PUT request.');
