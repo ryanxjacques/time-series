ffae2ae66185066c90e7f5d0c3758a2b69eb80ca
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-18 15:47:59 -0700

    finishing touches

--- server/routes/file.js
+++ server/routes/file.js
@@ -2,7 +2,19 @@ const express = require('express');
 const file = express.Router();
 
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({storage})
 
 file.post('/', upload.single('uploaded_file'), function (req, res) {
   // req.file is the name of your file in the form above, here 'uploaded_file'
--- web_app/js_modules/ajax_modules/fetch.js
+++ web_app/js_modules/ajax_modules/fetch.js
@@ -1,5 +1,4 @@
-// const fileInput = document.getElementById('myFile'); // get the input element for the file
-
+//Credit: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 async function upload(formData) {
   try {
     const response = await fetch("https://35.85.29.142:3000/file", {
@@ -13,19 +12,6 @@ async function upload(formData) {
   }
 }
 
-const fileInput = document.getElementById('myFile'); // get the input element for the file
-fileInput.onchange = () => {
-  const selectedFile = fileInput.files[0];
-  console.log(selectedFile);
-  const formData = new FormData();
-  formData.append("username", "abc123");
-  formData.append("file", selectedFile);
-  upload(formData);
-}
-
-
-//Credit: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-
 async function postData(url, data) {
   const response = await fetch(url, {
     method: "POST", 
@@ -37,27 +23,14 @@ async function postData(url, data) {
   return response.json();
 }
 
-// async function upload(url, formData) {
-//   try {
-//     const response = await fetch(url, {
-//       method: "PUT",
-//       body: formData,
-//     });
-//     const result = await response.json();
-//     console.log("Success:", result);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
-
-// const formData = new FormData();
-// const fileField = document.getElementById('logo-image');
-
-// formData.append("test", "abc123");
-// formData.append("avatar", fileField.files);
-
-// upload('https://35.85.29.142:3000/', formData);
-
+const fileInput = document.getElementById('myFile');
+fileInput.onchange = () => {
+  const selectedFile = fileInput.files[0];
+  console.log(selectedFile);
+  const formData = new FormData();
+  formData.append("uploaded_file", selectedFile);
+  upload(formData);
+}
 
 postData("https://35.85.29.142:3000/", { message: "Test: connect to / => PASSED" }).then((data) => {
   console.log(data); // JSON data parsed by `data.json()` call
