bfe33085f27833bda05e949dd0b0a192c9b983d7
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-17 19:40:21 -0700

    trying to make https server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
+// Requiring in-built https for creating
+// https server
 const https = require("https");
 
 const express = require('express');
 const app = express();
 
+// Requiring file system to use local files
+const fs = require("fs");
+
+const bodyParser = require("body-parser");
+// Configuring express to use body-parser
+// as middle-ware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+const options = {
+  key: fs.readFileSync("etc/pki/tls/certs/custom.key"),
+  cert: fs.readFileSync("etc/pki/tls/certs/localhost.crt"),
+};
 
 const port = process.env.PORT || 3000;
 https.createServer(options, app)
