79eb9d23b6358ba4c1fa0af5c81e50800bc18d2e
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-17 18:52:12 -0700

    fixed small typo

--- server/server.js
+++ server/server.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Server started on port ${port}');
+  console.log(`Server started on ${port}`);
 });
