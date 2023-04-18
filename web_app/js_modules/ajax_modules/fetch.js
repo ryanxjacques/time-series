4c19909fdde2f3e13779d5b922f4bf254783fdc5
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-18 13:52:56 -0700

    successfully integrated Fetch API

--- web_app/js_modules/ajax_modules/fetch.js
+++ web_app/js_modules/ajax_modules/fetch.js
@@ -10,6 +10,10 @@ async function postData(url = "", data = {}) {
   return response.json();
 }
 
-postData("https://35.85.29.142:3000/", { message: "Hello, world." }).then((data) => {
+postData("https://35.85.29.142:3000/", { message: "Test: connect to / => PASSED" }).then((data) => {
+  console.log(data); // JSON data parsed by `data.json()` call
+});
+
+postData("https://35.85.29.142:3000/login", { message: "Test: connect to /login => PASSED" }).then((data) => {
   console.log(data); // JSON data parsed by `data.json()` call
 });
