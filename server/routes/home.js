e1e1572b6d91e010928e87435cbdbf45839edda2
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-18 14:35:55 -0700

    patching bugs

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,5 +1,33 @@
+const express = require('express');
+const home = express.Router();
+
 // <======= NEW STUFF =======>
 const multer = require('multer');
+
+const imageStorage = multer.diskStorage({
+  // Destination to store image     
+  destination: 'images', 
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '_' + Date.now() 
+           + path.extname(file.originalname))
+          // file.fieldname is name of the field (image)
+          // path.extname get the uploaded file extension
+  }
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  limits: {
+    fileSize: 1000000 // 1000000 Bytes = 1 MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) { 
+       // upload only png and jpg format
+       return cb(new Error('Please upload a Image'))
+     }
+   cb(undefined, true)
+}
+});
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
@@ -13,8 +41,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 // <======= END =======>
 
-const express = require('express');
-const home = express.Router();
 
 home.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -28,9 +54,16 @@ home.post('/', (req, res) => {
   res.send(JSON.stringify(msg));
 });
 
-home.put('/', upload.single('file'), function(req, res) {
-  console.log('recieved PUT request.');
-  res.send('File uploaded successfully');
+// For Single image upload
+home.put('/', imageUpload.single('image'), (req, res) => {
+  res.send(req.file)
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message })
 });
+
+// home.put('/', upload.single('file'), function(req, res) {
+//   console.log('recieved PUT request.');
+//   res.send('File uploaded successfully');
+// });
 
 module.exports = home;
