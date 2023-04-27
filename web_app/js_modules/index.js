86b0702f86df79cd4f6e4efec4ef1fecb2fc06d5
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-04-27 10:38:11 -0700

    removed some artifact code

--- web_app/js_modules/index.js
+++ web_app/js_modules/index.js
@@ -1,17 +1,5 @@
-// const loginButton = document.getElementById("login-button");
-// const loginPopup = document.getElementById("login-popup-container");
 const submit = document.getElementById("submit-login-button");
 const searchButton = document.getElementById("search-button");
-
-// >> This feature will be removed 
-// loginButton.addEventListener("click", function() {
-//   loginPopup.style.display = "block";
-// });
-
-// submit.addEventListener("click", function() {
-//   loginPopup.style.display = "none";
-// });
-// << This feature will be removed 
 
 searchButton.addEventListener("click", function() {
   const searchValue = document.getElementById("search-input").value;
