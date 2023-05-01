96ddca36f3b5bd3b05a0c80e3ccec04078f5eb3e
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-05-01 16:36:04 -0700

    change

--- server/python_modules/main.py
+++ server/python_modules/main.py
@@ -13,7 +13,7 @@ import graph_display as gd
 
 cnx = mysql.connector.connect(user=os.environ.get("DB_USER"), password=os.environ.get("DB_PASS"),
                               host=os.environ.get("DB_HOST"),
-                              database='time-series')
+                              database='time_series')
 
 # upload dir: /var/www/html/uploads
 def watch_directory():
