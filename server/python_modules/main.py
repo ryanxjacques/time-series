18c5116a74d54448cd8a89348e5b75ac0a86fc46
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-05-02 12:46:11 -0700

    revert sql testing

--- server/python_modules/main.py
+++ server/python_modules/main.py
@@ -9,7 +9,6 @@ import json
 import sys
 import regex as re
 import mysql.connector
-from sqlalchemy import create_engine
 
 # Import Local Files
 import config
@@ -22,10 +21,6 @@ cnx = mysql.connector.connect(
     password=os.environ.get("DB_PASS"),
     host=os.environ.get("DB_HOST"),
     database='time_series')
-
-
-# Create SQLAlchemy engine with MySQL dialect and connection object
-engine = create_engine('mysql+mysqlconnector://', echo=False, connect_args={'con': cnx})
 
 # Create cursor object.
 cursor = cnx.cursor()
@@ -150,7 +145,7 @@ def process_file(filename, path_to_file):
     sql_data = data.rename(columns=dict(zip(data.columns, new_column_names)))
     print(f"New sql data: {sql_data}")
 
-    sql_data.to_sql(name='ts_data',con=engine,index=False)
+    sql_data.to_sql(name='ts_data',con=cnx,index=False)
     log(f"{filename} was converted to SQL")
     cnx.close()
 
