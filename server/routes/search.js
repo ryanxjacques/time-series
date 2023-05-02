04272ed4ada46e5e977dfa17c4c4bba60f15e451
Sterling Stewart <sstewar9@is-shell01.uoregon.edu>
2023-05-01 21:02:35 -0700

    trying to fix search

--- server/routes/search.js
+++ server/routes/search.js
@@ -11,15 +11,8 @@ search.get('/timeseries', async (req, res) => {
     const connection = db.connectToDataBase('time_series');
     const searchTerm = req.query.query;
     const fields = ['ts_id', 'ts_name', 'ts_desc', 'ts_domain', 'ts_units', 'ts_keywords'];
-    const unique_id = {
-        ts_id: searchTerm,
-        ts_name: `%${searchTerm}%`,
-        ts_desc: `%${searchTerm}%`,
-        ts_domain: `%${searchTerm}%`,
-        ts_units: `%${searchTerm}%`,
-        ts_keywords: `%${searchTerm}%`
-    };
-    const timeseries_res = await db.getRecordElement(connection, 'ts_metadata', fields, unique_id);
+    const conditions = `ts_id = ${searchTerm}`;
+    const timeseries_res = await db.getRecordElement(connection, 'ts_metadata', fields, conditions);
     db.disconnect(connection);
     res.json(timeseries_res);
 })
@@ -28,11 +21,8 @@ search.get('/users', async (req, res) => {
     const connection = db.connectToDataBase('users');
     const searchTerm = req.query.query;
     const fields = ['id', 'username'];
-    const unique_id = {
-        id: searchTerm,
-        username: `%${searchTerm}%`
-    };
-    const users_res = await db.getRecordElement(connection, 'users', fields, unique_id);
+    const conditions = `username LIKE '%${searchTerm}%'`;
+    const users_res = await db.getRecordElement(connection, 'users', fields, conditions);
     db.disconnect(connection);
     res.json(users_res);
 })
