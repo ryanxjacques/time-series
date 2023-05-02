840b4d75744a4aef32ddaee493899516d048f9c0
Sterling Stewart <sstewar9@is-shell01.uoregon.edu>
2023-05-01 18:35:51 -0700

    fixing routing

--- server/routes/browse.js
+++ server/routes/browse.js
@@ -7,14 +7,14 @@ const timeseries = express.Router();
 
 const db = require('../js_modules/database');
 
-timeseries.get('https://35.85.29.142:3000/timeseries', async (req, res) => {
+browse.get('https://35.85.29.142:3000/timeseries', async (req, res) => {
     const connection = db.connectToDataBase('time_series');
     const timeseries_res = await db.getRecordElement(connection, 'ts_metadata', ['ts_id', 'ts_name', 'ts_desc', 'ts_domain', 'ts_units', 'ts_keywords'], true);
     db.disconnect(connection);
     res.json(timeseries_res);
 })
 
-users.get('https://35.85.29.142:3000/users', async (req, res) => {
+browse.get('https://35.85.29.142:3000/users', async (req, res) => {
     const connection = db.connectToDataBase('users');
     const users_res = await db.getRecordElement(connection, 'users', ['id', 'username'], true);
     db.disconnect(connection);
