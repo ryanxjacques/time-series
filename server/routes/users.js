e4cc9c5a3bf0669cea4abce3c5ddb41ccc5d762e
Sterling Stewart <sstewar9@is-shell01.uoregon.edu>
2023-05-01 17:32:09 -0700

    Testing php to html/js

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,7 +9,7 @@ const db = require('../js_modules/database');
 
 users.get('/', async (req, res) => {
     const connection = db.connectToDataBase('users');
-    const users_res = await db.getRecordElement(connection, 'users', 'id, username', true);
+    const users_res = await db.getRecordElement(connection, 'users', ['id', 'username'], true);
     db.disconnect(connection);
     res.json(users_res);
 })
