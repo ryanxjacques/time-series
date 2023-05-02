d55a054abecce712493671dfc565404544a71561
Sterling Stewart <sstewar9@is-shell01.uoregon.edu>
2023-05-01 17:34:39 -0700

    Testing php to html/js

--- server/routes/timeseries.js
+++ server/routes/timeseries.js
@@ -9,7 +9,7 @@ const db = require('../js_modules/database');
 
 timeseries.get('/', async (req, res) => {
     const connection = db.connectToDataBase('time_series');
-    const timeseries_res = await db.getRecordElement(connection, 'ts_metadata', '*', {});
+    const timeseries_res = await db.getRecordElement(connection, 'ts_metadata', '*', true);
     db.disconnect(connection);
     res.json(timeseries_res);
 })
