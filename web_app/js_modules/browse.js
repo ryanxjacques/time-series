f41f8425d8dc37b4521a5edef7f609dc7601e559
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-05-03 12:29:39 -0700

    making progress

--- web_app/js_modules/browse.js
+++ web_app/js_modules/browse.js
@@ -69,7 +69,9 @@ function displayTsMetadata(ts_metadata) {
     idLink.textContent = ts.ts_id;
     idLink.href = `view-time-series.html?ts_id=${ts.ts_id}`;
     idLink.addEventListener("click", function() {
-      sendRequest("POST", "https://35.85.29.142:3000/download", {id: ts.ts_id}); 
+      sendRequest("POST", "https://35.85.29.142:3000/download", {id: ts.ts_id}).then(response => {
+        alert("testing");
+      }); 
     });
     idCell.appendChild(idLink);
     row.appendChild(idCell);
