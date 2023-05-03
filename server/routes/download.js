ab0d13d77d39e407460da88ff0da9a089172de2a
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-05-03 10:19:59 -0700

    making progress

--- server/routes/download.js
+++ server/routes/download.js
@@ -9,50 +9,49 @@ const jsonexport = require('jsonexport');
 
 // needs to change to a POST request.
 download.get('/', (req, res) => {
-  downloadFile('470')
-  // .then(response => {
-  //   const filePath = '/var/www/html/downloads/data.csv';
-  //   sendFile(filePath);
-  // });
+  downloadFile('470').then(response => {
+
+  });
 });
 
 
 const downloadFile = (id) => {
-  db.getFirstRecord(connection, 'ts_data', {ts_id: id}).then(response => {
-    const record = response[0];
-    let count = 0;
-    for (let column in record) {
-      if (record[column] != null) {
-        count++
+  const promise = new Promise((resolve, reject) => {
+    db.getFirstRecord(connection, 'ts_data', {ts_id: id}).then(response => {
+      const record = response[0];
+      let count = 0;
+      for (let column in record) {
+        if (record[column] != null) {
+          count++
+        }
       }
-    }
 
-    count -= 2; // subtract ts_id and ts_datetime columns;
+      count -= 2; // subtract ts_id and ts_datetime columns;
 
-    const query = ['ts_datetime'];
-    for(let i = 1; i <= count; i++) {
-      // push is javascript's append method for lists
-      query.push(`ts_magnitude${i}`);
-    }
-    return db.getRecordElement(connection, 'ts_data', query, {ts_id: id});
-  }).then(response => {
-    // Convert mySQL data into CSV
-    return jsonexport(response);
-  }).then(response => {
-    // save the CSV data to a file
-    const promise = new Promise((resolve, reject) => {
+      const query = ['ts_datetime'];
+      for(let i = 1; i <= count; i++) {
+        // push is javascript's append method for lists
+        query.push(`ts_magnitude${i}`);
+      }
+      return db.getRecordElement(connection, 'ts_data', query, {ts_id: id});
+    }).then(response => {
+      // Convert mySQL data into CSV
+      return jsonexport(response);
+    }).then(response => {
+      // save the CSV data to a file
       fs.writeFile('/var/www/html/downloads/data.csv', response, (err) => {
         if (err) {
           reject('Error writing file:' + err);
         }
         resolve(true);
       });
+    }).catch(error => {
+      console.error(error);
     });
     return promise;
-  }).catch(error => {
-    console.error(error);
   });
 }
+
 
 const sendFile = (filePath) => {
   fs.readFile(filePath, (err, data) => {
@@ -66,7 +65,8 @@ const sendFile = (filePath) => {
     res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
     res.send(data);
   });
-};
+}
+
 
 // Export file object for server.js to use.
 module.exports = download;
