da8ddb16cb40206f05a12839ffc09bfb57eb5b48
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-05-02 22:31:45 -0700

    Update main.py

--- server/python_modules/main.py
+++ server/python_modules/main.py
@@ -244,6 +244,8 @@ def process_file(filename, path_to_file):
     cursor.execute(query, (contributor_id, ts_name))
     result = cursor.fetchall()
 
+    print(result)
+
     # store the metadata in individual variables
     if result is not None:
         session_id = result[0]
