15b2c3611788621b223b8abbea13a57b6e821a9a
Sterling Stewart <sstewar9@is-shell01.uoregon.edu>
2023-05-02 19:17:26 -0700

    testing view pages

--- server/routes/view.js
+++ server/routes/view.js
@@ -7,7 +7,8 @@ view.get('/timeseries', async (req, res) => {
     const connection = db.connectToDataBase('time_series');
     const ts_id = req.query.ts_id;
     const fields = ['ts_id', 'ts_name', 'ts_desc', 'ts_domain', 'ts_units', 'ts_keywords'];
-    const timeseries_res = await db.getRecordElement(connection, 'ts_metadata', fields, {ts_id: ts_id});
+    const condition = `ts_id = '${ts_id}`;
+    const timeseries_res = await db.getRecordElement(connection, 'ts_metadata', fields, condition);
     db.disconnect(connection);
     res.json(timeseries_res);
 });
@@ -16,7 +17,8 @@ view.get('/users', async (req, res) => {
     const connection = db.connectToDataBase('users');
     const user_id = req.query.user_id;
     const fields = ['id', 'username'];
-    const user_res = await db.getRecordElement(connection, 'users', fields, {user_id: user_id});
+    const condition = `tuser_id = '${user_id}`;
+    const user_res = await db.getRecordElement(connection, 'users', fields, condition);
     db.disconnect(connection);
     res.json(user_res);
 });
