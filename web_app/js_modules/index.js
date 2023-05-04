6b3bc6e96486282ca498e4d20b14b21e4c416dd1
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2023-05-04 15:58:10 -0700

    new login page!

--- web_app/js_modules/index.js
+++ web_app/js_modules/index.js
@@ -1,3 +1,16 @@
+/* --------------------------------------------------------------------------- */
+/* ---------------------------- Document Elements ---------------------------- */
+
+const textInputs = document.querySelectorAll('.text-input');
+const textLabels = document.querySelectorAll('.text-input-label');
+const loginBtn = document.querySelector('.button-input');
+const loginContainer = document.querySelector('.login-container');
+const registerLink = document.querySelector('#register-link');
+
+/* --------------------------------------------------------------------------- */
+/* ------------------------------ Fetch Requests ----------------------------- */
+
+// Uuid Request
 const getUuid = () => {
   sendRequest('GET', 'https://35.85.29.142:3000/uuidGen/').then(response => {
     localStorage.setItem('uuid', response.id);
@@ -5,17 +18,21 @@ const getUuid = () => {
 }
 
 const checkActive = (uuid) => {
+  // check if active
   sendRequest('POST', 'https://35.85.29.142:3000/auth/is-active?', {uuid: uuid}).then(response => {
     if (response.status) {
+      // If active, get username.
       return sendRequest('POST', 'https://35.85.29.142:3000/auth/username', {uuid: uuid});
     }
   }).then(response => {
+    console.log(response);
     localStorage.setItem('username', response.username);
-    return window.location.replace("pages/home.html");
+    successfulLogin();
   });
 }
 
 const main = () => {
+  // This get's execute when page loads.
   const uuid = localStorage.getItem('uuid')
   if (uuid) {
     checkActive(uuid);
@@ -24,5 +41,217 @@ const main = () => {
   }
 };
 
-// Execute main
-main();
+// Login Request
+loginBtn.addEventListener("click", (event) => {
+  // Default
+  let missingData = false;
+
+  // Aliases
+  const usernameContainer = textInputs[0];
+  const passwordPassword = textInputs[1];
+  const username = textInputs[0].value;
+  const password = textInputs[1].value;
+  const usernameLabel = textLabels[0];
+  const passwordLabel = textLabels[1];
+
+  // Check for missing username
+  if (!username) {
+    missingData = true;
+    invalid_empty_input(usernameContainer, usernameLabel);
+  } else {
+    valid_input(usernameContainer, usernameLabel);
+  }
+
+  // Check for missing password
+  if (!password) {
+    missingData = true;
+    invalid_empty_input(passwordPassword, passwordLabel);
+  } else {
+    valid_input(passwordPassword, passwordLabel);
+  }
+
+  if(!missingData) {
+    message = {'username': username, 'password': password, 'uuid': localStorage.getItem('uuid')};
+    sendRequest("POST", "https://35.85.29.142:3000/auth/login", message).then((data) => {
+      if (data.status) {
+        successfulLogin();
+      } else {
+        failedLogin();
+        invalid_empty_input(usernameContainer, usernameLabel);
+        invalid_empty_input(passwordPassword, passwordLabel);
+      }
+    });
+  }
+});
+
+const invalid_empty_input = (container, text) => {
+  container.classList.add('invalid-input-container');
+  text.classList.add('invalid-input-text');
+};
+
+const valid_input = (container, text) => {
+  container.classList.remove('invalid-input-container');
+  text.classList.remove('invalid-input-text');
+}
+
+/* --------------------------------------------------------------------------- */
+/* ---------------------------- Font Size Control ---------------------------- */
+
+const caclulateFontSize = () => {
+  const height = window.getComputedStyle(textInputs[0]).getPropertyValue('height');
+  const fontSize = parseFloat(height) * 0.30;
+  for(let i = 0; i < textInputs.length; i++) {
+    textLabels[i].style.fontSize = `${fontSize}px`;
+    textInputs[i].style.fontSize = `${fontSize}px`;
+  }
+  loginBtn.style.fontSize = `${fontSize}px`;
+}
+
+const calculateLabelOffset = () => {
+  const xPos = textInputs[0].offsetLeft;
+  for(let i = 0; i < textLabels.length; i++) {
+    // equation is: text-input.left + text-input.padding-left.
+    textLabels[i].style.left = `${xPos + 40}px`
+  }
+}
+
+window.addEventListener('resize', (event) => {
+  caclulateFontSize();
+  calculateLabelOffset();
+});
+
+window.onload = () => {
+  main();
+  caclulateFontSize();
+  calculateLabelOffset();
+};
+
+/* ------------------------------------------------------------------------- */
+/* --------------------------- Animation Control --------------------------- */
+
+// Username Animations
+textInputs[0].addEventListener('focus', () => {
+  textLabels[0].classList.add('forward-label-animation');
+  textLabels[0].classList.remove('backward-label-animation');
+});
+
+textInputs[0].addEventListener('blur', () => {
+  if (!textInputs[0].value) {
+    textLabels[0].classList.add('backward-label-animation');
+    textLabels[0].classList.remove('forward-label-animation');
+  }
+});
+
+textLabels[0].addEventListener("animationend", (event) => {
+  if (event.animationName === "move-backwards") {
+    textLabels[0].classList.remove('backward-label-animation');
+    textLabels[0].classList.remove('forward-label-animation');
+  }
+});
+
+// Password Animations
+textInputs[1].addEventListener('focus', () => {
+  textLabels[1].classList.add('forward-label-animation');
+  textLabels[1].classList.remove('backward-label-animation');
+});
+textInputs[1].addEventListener('blur', () => {
+  if (!textInputs[1].value) {
+    textLabels[1].classList.add('backward-label-animation');
+    textLabels[1].classList.remove('forward-label-animation');
+  }
+});
+textLabels[1].addEventListener("animationend", (event) => {
+  if (event.animationName === "move-backwards") {
+    textLabels[1].classList.remove('backward-label-animation');
+    textLabels[1].classList.remove('forward-label-animation');
+  }
+});
+
+// Login Button Animations
+loginBtn.addEventListener("mouseover", () => {
+  loginBtn.classList.add('forward-button-animation');
+  loginBtn.classList.remove('backward-button-animation');
+});
+
+loginBtn.addEventListener("mouseout", () => {
+  if (loginBtn !== document.activeElement) {
+    loginBtn.classList.add('backward-button-animation');
+    loginBtn.classList.remove('forward-button-animation');
+  }
+  // Smoother animation if invalid login. 
+  loginBtn.blur();
+});
+
+loginBtn.addEventListener('focus', () => {
+  loginBtn.classList.add('forward-button-animation');
+  loginBtn.classList.remove('backward-button-animation');
+});
+
+loginBtn.addEventListener('blur', () => {
+  loginBtn.classList.add('backward-button-animation');
+  loginBtn.classList.remove('forward-button-animation');
+});
+
+loginBtn.addEventListener("animationend", (event) => {
+  if (event.animationName === "hover-backwards") {
+    loginBtn.classList.remove('forward-button-animation');
+    loginBtn.classList.remove('backward-button-animation');
+  }
+});
+
+// Login Container Animations
+const successfulLogin = () => {
+  loginContainer.classList.add('slide-left');
+  loginBtn.classList.add('forward-button-animation');
+  loginBtn.classList.remove('backward-button-animation');
+  loginBtn.style.cursor = "default";
+  loginBtn.style.outline = "none";
+}
+
+const failedLogin = () => {
+  // This starts the sequence.
+  loginContainer.classList.add('slide-error-left');
+  // Delete username and password from text-input.
+  textInputs[0].value = '';
+  textInputs[1].value = '';
+  textLabels[0].classList.remove('forward-label-animation');
+  textLabels[0].classList.add('backward-label-animation');
+  textLabels[1].classList.remove('forward-label-animation');
+  textLabels[1].classList.add('backward-label-animation');
+  textInputs[0].blur();
+  textInputs[1].blur();
+}
+
+registerLink.addEventListener("click", () => {
+  loginContainer.classList.add('slide-right');
+  registerLink.style.cursor = "default";
+});
+
+registerLink.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) { // 13 is the code for the "Enter" key
+    loginContainer.classList.add('slide-right');
+    registerLink.style.outline = "none";
+    registerLink.style.cursor = "default";
+  }
+});
+
+loginContainer.addEventListener("animationend", (event) => {
+  if (event.animationName === "slide-left") {
+    window.location.replace("pages/home.html");
+  }
+  else if (event.animationName === "slide-right") {
+    window.location.replace("pages/signup.html");
+  } 
+  // Error animation sequence
+  else if (event.animationName === "slide-error-left") {
+    loginContainer.classList.remove('slide-error-left');
+    loginContainer.classList.add('slide-error-right');
+  }
+  else if (event.animationName === "slide-error-right") {
+    loginContainer.classList.remove('slide-error-right');
+    loginContainer.classList.add('slide-error-center');
+  }
+  else if (event.animationName === "slide-error-center") {
+    loginContainer.classList.remove('slide-error-center');
+  }
+});
