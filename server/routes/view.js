9c730406a56b624e034da39c67361bf3f3b5455e
Sterling Stewart <sstewar9@is-shell01.uoregon.edu>
2023-05-04 20:58:59 -0700

    List submitted time series on user profile

--- server/routes/view.js
+++ server/routes/view.js
@@ -15,14 +15,20 @@ view.get('/timeseries', async (req, res) => {
 });
 
 view.get('/users', async (req, res) => {
-    const connection = db.connectToDataBase('users', 'view.js->time_series ');
+    const user_connection = db.connectToDataBase('users', 'view.js->users ');
+    const ts_connection = db.connectToDataBase('time_series', 'view.js->users ');
     const user_id = req.query.user_id;
-    const fields = ['id', 'username'];
-    const condition = 'id = ?';
+    const user_fields = ['id', 'username'];
+    const user_condition = 'id = ?';
+    const ts_fields = ['ts_id', 'ts_name', 'ts_desc', 'ts_domain', 'ts_units', 'ts_keywords'];
+    const ts_condition = 'ts_contributor = ?';
     const id = [user_id];
-    const user_res = await db.getRecordByCondition(connection, 'users', fields, condition, id);
-    db.disconnect(connection);
-    res.json(user_res);
+    const user_res = await db.getRecordByCondition(user_connection, 'users', user_fields, user_condition, id);
+    db.disconnect(user_connection);
+    const ts_res = await db.getRecordByCondition(ts_connection, 'time_series', ts_fields, ts_condition, id);
+    db.disconnect(ts_connection);
+    result = [user_res, ts_res];
+    res.json(result);
 });
 
 module.exports = view;
